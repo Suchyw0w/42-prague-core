dcdb4f8da2af4562196ce6bb382c0a72651ea5df
Ondřej Suchánek <ondrasuchos@gmail.com>
2023-01-16 08:38:32 +0100

    gitignore, comments

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: osuchane <osuchane@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 16:28:21 by osuchane          #+#    #+#             */
-/*   Updated: 2023/01/15 16:10:49 by osuchane         ###   ########.fr       */
+/*   Updated: 2023/01/15 16:43:36 by osuchane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ char	*ft_emptystr(void)
 {
 	char	*ptr;
 
+	// Allocate memory for a single character
 	ptr = malloc(1 * sizeof(char));
 	if (!ptr)
 		return (NULL);
+	// Assign null character to the allocated memory
 	ptr[0] = '\0';
 	return (ptr);
 }
@@ -28,13 +30,15 @@ int	ft_is_in_set(char c, char const *set)
 	unsigned int	i;
 
 	i = 0;
+	// Loop as long as current character of set is not null
 	while (set[i])
 	{
+		// Check if the current character is equal to the character in the set
 		if (c == set[i])
-			return (1);
+			return (1); // If the characters match, return 1
 		i++;
 	}
-	return (0);
+	return (0);  // If the loop completes without finding a match, return 0
 }
 
 int	ft_count(char const *s1, char const *set)
@@ -46,7 +50,7 @@ int	ft_count(char const *s1, char const *set)
 	start = 0;
 	i = 0;
 	end = 0;
-	while (ft_is_in_set(s1[i], set))
+	while (ft_is_in_set(s1[i], set)) // Loop as long as the current character of s1 is in the set
 	{
 		start++;
 		i++;
@@ -55,7 +59,7 @@ int	ft_count(char const *s1, char const *set)
 		i++;
 	while (ft_is_in_set(s1[i - end - 1], set) && (i - end - 1) > 0)
 		end++;
-	return (i - (start + end));
+	return (i - (start + end));// Return the length of the string s1 minus the number of leading and trailing characters that need to be trimmed
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
@@ -65,23 +69,23 @@ char	*ft_strtrim(char const *s1, char const *set)
 	int		k;
 	char	*ptr;
 
-	if (!s1 || !set)
+	if (!s1 || !set) // Check if either one of the input parameters s1 or set are null
 		return (NULL);
-	i = ft_count(s1, set);
+	i = ft_count(s1, set); // Get the length of the resulting trimmed string
 	if (i < 1)
-		return (ft_emptystr());
-	ptr = malloc((i + 1) * sizeof(char));
+		return (ft_emptystr()); // If the length is less than 1, return an empty string
+	ptr = malloc((i + 1) * sizeof(char)); // Allocate memory for the resulting string
 	if (!ptr)
 		return (NULL);
 	j = 0;
 	k = 0;
-	while (ft_is_in_set(s1[j], set))
+	while (ft_is_in_set(s1[j], set)) // Loop as long as the current character of s1 is in the set
 		j++;
 	while (k < i)
 	{
-		ptr[k] = s1[j + k];
+		ptr[k] = s1[j + k]; // Assign the current character of s1 to the current index of the resulting string
 		k++;
 	}
-	ptr[k] = '\0';
-	return (ptr);
+	ptr[k] = '\0'; // Assign null character at the end of the resulting string
+	return (ptr); // Return the pointer to the resulting string
 }
